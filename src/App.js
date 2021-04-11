de049fc92737cc03eb42d955879da85cb44c8e44
nadiia <nadiia.oleks@gmail.com>
2021-04-11 18:42:32 +0300

    functionality of adding new tasks is ready

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
 
 const App = () => {  
   const [tasks, setTasks] = useState([
@@ -23,18 +24,31 @@ const App = () => {
         reminder: false
     }
 ])
-
+//Add task
+const addTask = (task) => {
+const id = Math.floor(Math.random() * 10000) + 1;
+const newTask = { id, ...task};
+setTasks([...tasks, newTask])
+}
 //Delete task
 const deleteTask = (id) => {
   setTasks(tasks.filter((task) => task.id !== id))
 }
+
+//Toggle reminder
+const toggleReminder = (id) => {
+  console.log(id)
+  setTasks(tasks.map((task) => task.id === id 
+  ? { ...task, reminder: !task.reminder } : task))
+}
+
   return (
     <div className="container">
      <Header />
-     {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask}/>
+     <AddTask onAdd={addTask}/>
+     {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
       : "No Tasks To Show"}
     </div>
   );
 }
-
 export default App;
