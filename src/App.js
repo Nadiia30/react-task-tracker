8d28e37dca91ccec2ac6d91e11b61d65a10487b2
nadiia <nadiia.oleks@gmail.com>
2021-04-13 12:51:48 +0300

    add tasks to server, toggle reminder on server

--- src/App.js
+++ src/App.js
@@ -23,11 +23,28 @@ const App = () => {
     return data;
    }
 
+  //Fetch task
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`);
+    const data = await res.json();
+    return data;
+   }
+
   //Add task
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 10000) + 1;
-    const newTask = { id, ...task};
-    setTasks([...tasks, newTask])
+  const addTask = async (task) => {
+    const res = await fetch('http://localhost:5000/tasks',
+    {
+      method: 'POST',
+      headers: {
+        'Content-type':'application/json'
+      },
+      body: JSON.stringify(task)
+    })
+    const data = await res.json();
+    setTasks([...tasks,data])
+    // const id = Math.floor(Math.random() * 10000) + 1;
+    // const newTask = { id, ...task};
+    // setTasks([...tasks, newTask])
   }
 
   //Delete task
@@ -39,10 +56,22 @@ const App = () => {
   }
 
   //Toggle reminder
-  const toggleReminder = (id) => {
-    console.log(id)
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id);
+    const updTask = {...taskToToggle,
+       reminder:!taskToToggle.reminder};
+    const res = await fetch(`http://localhost:5000/tasks/${id}`,{
+      method: 'PUT',
+      headers:{
+        'Content-type':'application/json'
+      },
+      body:JSON.stringify(updTask)
+    })
+
+    const data = await res.json()
+  
     setTasks(tasks.map((task) => task.id === id 
-    ? { ...task, reminder: !task.reminder } : task))
+    ? { ...task, reminder: data.reminder } : task))
   }
 
     return (
